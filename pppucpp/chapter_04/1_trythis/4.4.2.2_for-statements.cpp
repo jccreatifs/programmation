63f5e7aec98b616638d08412289b165013c44c1e
xFAQ42 <email@jccesar.com>
2022-09-25 01:49:40 +0400

    Added Ch04TT entry

---
+++ pppucpp/chapter_04/1_trythis/4.4.2.2_for-statements.cpp
@@ -0,0 +1,28 @@
+/* Programming Principles and Practicecs Using C++
+ * Chapter 4 Try This section
+ * 
+ * A program that converts pesos ('P'), yen ('Y'), and won ('W') into dollars.
+ */
+
+#include "../../_/std_lib_facilities.h"
+
+int main()
+{
+/***
+// This block links this program to the input and output files
+// I'm using for testing.
+#ifndef ONLINE_JUDGE
+    freopen("input", "r", stdin);
+    freopen("output", "w", stdout);
+#endif
+***/
+// Solution starts here
+  char letter {' '};
+  int i {0};
+  for (i = 32; i <= 127; i++){
+    letter = i;
+    std::cout << letter << "\t"
+              << letter + 0 << "\n";
+  }
+  return 0;
+}
