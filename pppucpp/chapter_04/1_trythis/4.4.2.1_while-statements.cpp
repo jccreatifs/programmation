f3d9589ff03bba6ceef4e326f6e6bac3808af232
xFAQ42 <mail@jccesar.com>
2022-09-25 04:42:39 +0400

    Adjusted to work in ST4

--- pppucpp/chapter_04/1_trythis/4.4.2.1_while-statements.cpp
+++ pppucpp/chapter_04/1_trythis/4.4.2.1_while-statements.cpp
@@ -1,21 +1,20 @@
 /* Programming Principles and Practicecs Using C++
  * Chapter 4 Try This section
  * 
- * A program that create a list of lowercase letters and print their int value.s.
+ * A program that create a list of lowercase letters and print their int values.
  */
 
-#include "../../_/std_lib_facilities.h"
+#include "std_lib_facilities.h"
 
 int main()
 {
-/***
 // This block links this program to the input and output files
 // I'm using for testing.
 #ifndef ONLINE_JUDGE
     freopen("input", "r", stdin);
     freopen("output", "w", stdout);
 #endif
-***/
+
 // Solution starts here
   char letter {'a'};
 //  int i {0};
--- pppucpp/chapter_04/1_trythis/4.4.2.2_for-statements.cpp
+++ pppucpp/chapter_04/1_trythis/4.4.2.2_for-statements.cpp
@@ -2,20 +2,21 @@
  * Chapter 4 Try This section
  * 
  * A program that lists chars (letters, numbers, and spécial characters) and their int équivalent.
+ * https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
  */
 
-#include "../../_/std_lib_facilities.h"
+#include "std_lib_facilities.h"
 
 int main()
 {
-/***
+
 // This block links this program to the input and output files
 // I'm using for testing.
 #ifndef ONLINE_JUDGE
     freopen("input", "r", stdin);
     freopen("output", "w", stdout);
 #endif
-***/
+
 // Solution starts here
   char letter {' '};
   int i {0};
