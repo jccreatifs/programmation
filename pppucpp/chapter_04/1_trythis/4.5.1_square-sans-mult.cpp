446f07947c85810cf07e96a5dbbccc075f28fb4c
xFAQ42 <mail@jccesar.com>
2022-09-29 03:59:00 +0400

    Added Chapter 4 TT's square function without using multiplication

--- pppucpp/chapter_04/1_trythis/4.5.1_square-sans-mult.cpp
+++ pppucpp/chapter_04/1_trythis/4.5.1_square-sans-mult.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 
-int find_square(int n){
-  int square {0};
-  int i = n;
-  while (i != 0){
-    square = square + n;
-    i--;
-  }
-  return square;
-}
+
 
 int main(){
   std::cout << find_square(9) << std::endl;
